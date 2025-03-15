e025219acfe7f971bc93e1f8de701ea9422fc8db
killcerr <kill_cerr@outlook.com>
2025-03-15 12:54:54 +0800

    feat: read registry key to get known dlls

--- main.cpp
+++ main.cpp
@@ -9,31 +9,13 @@
 
 #define _AMD64_
 #include <libloaderapi.h>
+#include <winerror.h>
+#include <winreg.h>
 
-std::set<std::string> protectedDlls = {
-    "comctl32.dll",  "comdlg32.dll",
-    "crypt32.dll",   "imagehlp.dll",
-    "imm32.dll",     "kernelbase.dll",
-    "msctf.dll",     "msvcrt.dll",
-    "normaliz.dll",  "nsi.dll",
-    "oleaut32.dll",  "psapi.dll",
-    "shcore.dll",    "shell32.dll",
-    "shlwapi.dll",   "setupapi.dll",
-    "wintrust.dll",  "wldap32.dll",
-    "ws2_32.dll",    "advapi32.dll",
-    "bcrypt.dll",    "bcryptprimitives.dll",
-    "cfgmgr32.dll",  "clbcatq.dll",
-    "combase.dll",   "coml2.dll",
-    "difxapi.dll",   "gdi32.dll",
-    "gdi32full.dll", "gdiplus.dll",
-    "kernel32.dll",  "mscoree.dll",
-    "msvcp_win.dll", "ntdll.dll",
-    "ole32.dll",     "rpcrt4.dll",
-    "sechost.dll",   "ucrtbase.dll",
-    "user32.dll",    "win32u.dll",
-    "wow64.dll",     "wow64cpu.dll",
-    "wow64win.dll",  "msvcp140.dll",
-};
+#define MAX_KEY_LENGTH 255
+#define MAX_VALUE_NAME 16383
+
+std::set<std::string> knownDlls;
 
 std::string generate_single_function(std::string_view name,
                                      std::string_view exp) {
@@ -66,13 +48,66 @@ std::string lowerString(std::string_view str) {
   return res;
 }
 
+void initKnownDlls() {
+  HKEY hKey;
+  LPCSTR subKey =
+      R"(SYSTEM\CurrentControlSet\Control\Session Manager\KnownDLLs)";
+  DWORD dwDisposition;
+  LSTATUS status = RegCreateKeyExA(HKEY_LOCAL_MACHINE, subKey, 0, NULL,
+                                   REG_OPTION_NON_VOLATILE, KEY_READ, NULL,
+                                   &hKey, &dwDisposition);
+  char achKey[MAX_KEY_LENGTH];
+  DWORD cbName = 0;
+  char achClass[MAX_PATH]{0};
+  DWORD cchClassName = MAX_PATH;
+  DWORD cSubKeys = 0;
+  DWORD cbMaxSubKey = 0;
+  DWORD cchMaxClass = 0;
+  DWORD cValues = 0;
+  DWORD cchMaxValue = 0;
+  DWORD cbMaxValueData = 0;
+  DWORD cbSecurityDescriptor = 0;
+  FILETIME ftLastWriteTime;
+  DWORD i = 0, j = 0, retCode = 0;
+  char achValue[MAX_VALUE_NAME]{0};
+  DWORD cchValue = MAX_VALUE_NAME;
+  retCode = ::RegQueryInfoKeyA(hKey, achClass, &cchClassName, NULL, &cSubKeys,
+                               &cbMaxSubKey, &cchMaxClass, &cValues,
+                               &cchMaxValue, &cbMaxValueData,
+                               &cbSecurityDescriptor, &ftLastWriteTime);
+  if (cValues) {
+    for (i = 0; i < cValues; i++) {
+      cchValue = MAX_VALUE_NAME;
+      achValue[0] = '\0';
+      retCode =
+          ::RegEnumValue(hKey, i, achValue, &cchValue, NULL, NULL, NULL, NULL);
+      if (retCode == ERROR_SUCCESS) {
+        char data[1024]{0};
+        DWORD sz{1024};
+        RegGetValueA(hKey, NULL, achValue, RRF_RT_ANY, NULL, data, &sz);
+        knownDlls.insert(lowerString(data));
+      }
+    }
+  }
+  RegCloseKey(hKey);
+  std::cout << "known dlls:" << std::endl;
+  for (auto &dll : knownDlls) {
+    std::cout << "\t" << dll << std::endl;
+  }
+}
 int main(int argc, char **argv) {
+  initKnownDlls();
+  if (argc != 2) {
+    return 1;
+  }
   auto pe = LIEF::PE::Parser::parse(argv[1]);
   std::map<std::string, std::vector<std::string>> dlls;
   for (auto &import : pe->imports()) {
-    if (protectedDlls.contains(lowerString(import.name())))
+    if (knownDlls.contains(lowerString(import.name())))
       continue;
     if (import.name().starts_with("api-ms-win"))
+      continue;
+    if (lowerString(import.name()) == "msvcp140.dll")
       continue;
     std::cout << import.name() << std::endl;
     dlls[import.name()] = {};
